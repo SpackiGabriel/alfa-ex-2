011699e046a19c063f7c951ce07a65ba92b5d1cc
Gabriel Spacki <ifprgabriel@gmail.com>
2024-06-20 00:46:35 -0300

    :sparkles: feat: fetch recipes route

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException
+import requests
+
+app = FastAPI()
+
+@app.get('/recipes/{food}')
+async def get_recipe(food: str):
+    try:
+        url = f"https://forkify-api.herokuapp.com/api/search?q={food}"
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+        recipes = data.get('recipes')
+
+        if not recipes:
+            raise HTTPException(status_code=404, detail="Nenhuma receita encontrada.")
+
+        return {'count': len(recipes), 'recipes': recipes}
+
+    except requests.RequestException as e:
+        raise HTTPException(status_code=500, detail=f'Erro ao fazer requisição para a API do Forkify: {e}')
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Erro interno no servidor: {e}')
